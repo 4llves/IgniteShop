03f3f98a3e9c50d0bddb7eeaae98e41c3ceb3b6c
4llves <jhonatab4@gmail.com>
2024-07-18 19:02:04 -0300

    feat: implementing and configure stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -13,6 +14,11 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
+        />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+})
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <Button>
+      <span>lalala </span>
+      Hello World
+    </Button>
+  )
 }
