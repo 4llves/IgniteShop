826ea4e135640428dd667abe22cf8a6e3834eb5e
4llves <jhonatab4@gmail.com>
2024-08-02 21:43:18 -0300

    feat: structuring page product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Success() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,99</span>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero unde
+          provident ut fugiat possimus. Quae incidunt at est officia, culpa
+          assumenda blanditiis vel quidem nihil illum, pariatur tenetur, eaque
+          quam!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
