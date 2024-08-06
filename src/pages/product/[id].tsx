6cb8407f6e21cb8d69eff0f64366809daa9cf852
4llves <jhonatab4@gmail.com>
2024-08-05 23:59:53 -0300

    feat: add ssg in product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,11 @@
 // import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product'
+import { stripe } from '../../lib/stripe'
 
 export default function Success() {
   // const { query } = useRouter()
@@ -26,4 +28,19 @@ export default function Success() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expannd: ['default_price'],
+  })
+
+  return {
+    props: {},
+    revalidate: 60 * 60 * 1, // 1 hour save in chache
+  }
 }
