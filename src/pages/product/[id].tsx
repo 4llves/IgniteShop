71793408f41e20d692e82bdc8e0fc677f2687d9c
4llves <jhonatab4@gmail.com>
2024-08-07 19:01:12 -0300

    feat: add load info product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,23 +6,32 @@ import {
   ProductDetails,
 } from '../../styles/pages/product'
 import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+import Image from 'next/image'
 
-export default function Success() {
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Success({ product }: ProductProps) {
   // const { query } = useRouter()
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} alt="" width={520} height={480} />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,99</span>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero unde
-          provident ut fugiat possimus. Quae incidunt at est officia, culpa
-          assumenda blanditiis vel quidem nihil illum, pariatur tenetur, eaque
-          quam!
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
@@ -39,8 +48,21 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
     expannd: ['default_price'],
   })
 
+  const price = product.default_price as Stripe.Price
+
   return {
-    props: {},
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format((price.unit_amount as number) / 100),
+        description: product.description,
+      },
+    },
     revalidate: 60 * 60 * 1, // 1 hour save in chache
   }
 }
