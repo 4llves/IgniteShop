10809798508503f4cbc5a1c2c0cc9baf555eebd3
4llves <jhonatab4@gmail.com>
2024-08-11 21:55:31 -0300

    feat: structure page success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhulll, <strong>Alves Jhonata</strong>, sua Camiseta{' '}
+        <strong>xxx</strong> já está a caminho da sua casa! ✌️😎
+      </p>
+
+      <Link href="">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
