2b360be705d143bee12161bf587b1a3d055d3894
4llves <jhonatab4@gmail.com>
2024-08-12 16:19:39 -0300

    feat: implementing data in page success

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -16,9 +16,17 @@ export default async function handler(
     return res.status(400).json({ error: 'Price not found' })
   }
 
+  // const successUrl = `${process.env.NEXT_URL!}/success?session_id={CHECKOUT_SESSION_ID}`
+  // const cancelUrl = `${process.env.NEXT_URL!}/`
+
+  const successUrl = `http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `http://localhost:3000/`
+
+  // http://localhost:3000/success?session_id=cs_test_a1ifG2hvVcdJs4nYAcKmyRRQTqYrS8OwvzjCrtOFZBKJYZzwlaR5UifxVu
+
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: 'http://localhost:3000/success',
-    cancel_url: 'http://localhost:3000/',
+    success_url: successUrl,
+    cancel_url: cancelUrl,
     mode: 'payment',
     line_items: [
       {
