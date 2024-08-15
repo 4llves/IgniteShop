512ac6ae20cf66c2120658484ab0a49e1f7a5e35
4llves <jhonatab4@gmail.com>
2024-08-14 23:39:42 -0300

    feat: start challenge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import { ShoppingBag } from 'lucide-react'
 
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
@@ -12,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <img src={logoImg.src} alt="" />
+
+        <ShoppingBag />
       </Header>
 
       <Component {...pageProps} />
